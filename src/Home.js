d8c17f2e824126e691f247cdcc960f1d6cd69364
Devon Bahary <devon.bahary@robinhealthcare.com>
2021-04-28 22:23:51 -0700

    adds create new folder flow from <Home />

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import Backdrop from '@material-ui/core/Backdrop';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
@@ -13,6 +15,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { AppBar } from './common/AppBar';
 import { Content } from './common/Content';
 import { RouterUtil } from './utilities/RouterUtil';
+import { ApiUtil } from './utilities/ApiUtil';
 
 const useStyles = makeStyles(() => ({
     content: {
@@ -27,6 +30,66 @@ const useStyles = makeStyles(() => ({
     },
 }));
 
+const GetStartedDialog = ({ 
+    folderUUID,
+    handleCreateNewFolder,
+    handleFolderUUIDChange,
+    handleNavigate,
+    onClose,
+    open, 
+}) => {
+    const handleTextFieldKeyPress = (e) => {
+        if (e.key === 'Enter') handleNavigate();
+    };
+
+    const isCreateNewFolderButtonDisabled = Boolean(folderUUID.length);
+    const isNavigateButtonDisabled = !Boolean(folderUUID.length);
+
+    return (
+        <Dialog open={open} onClose={onClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Get Started</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    Enter the UUID of a folder you want to navigate to or create a new one.
+                </DialogContentText>
+                <TextField
+                    id="input-with-icon-textfield"
+                    fullWidth
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <FolderIcon />
+                            </InputAdornment>
+                        ),
+                    }}
+                    label="Folder UUID"
+                    onChange={handleFolderUUIDChange}
+                    onKeyPress={handleTextFieldKeyPress}
+                    value={folderUUID}
+                    variant="outlined"
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button 
+                    color="primary" 
+                    disabled={isCreateNewFolderButtonDisabled} 
+                    onClick={handleCreateNewFolder}
+                >
+                    Create A New Folder
+                </Button>
+                <Button 
+                    color="primary" 
+                    disabled={isNavigateButtonDisabled} 
+                    onClick={handleNavigate} 
+                    variant="contained"
+                >
+                    Navigate
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
 export const Home = () => {
     const [ isDialogOpen, setIsDialogOpen ] = useState(false);
     const openDialog = () => setIsDialogOpen(true);
@@ -37,57 +100,39 @@ export const Home = () => {
 
     const history = useHistory();
     const handleNavigate = () => RouterUtil.goToFolder(history, folderUUID);
-    const handleTextFieldKeyPress = (e) => {
-        if (e.key === 'Enter') handleNavigate();
-    };
+
+    const [ isLoadingNewFolder, setIsLoadingNewFolder ] = useState(false);
+    const handleCreateNewFolder = async () => {
+        setIsLoadingNewFolder(true);
+        const folder = await ApiUtil.createFolder();
+        RouterUtil.goToFolder(history, folder.uuid);
+    }
     
     const classes = useStyles();
-
-    const isNavigateButtonDisabled = !Boolean(folderUUID.length);
 
     return (
         <div>
             <AppBar title="deep-note" />
             <Content>
-                <div className={classes.home}>
-                    <Button color="primary" onClick={openDialog} variant="outlined">
-                        Get Started
-                    </Button>
-                    <Dialog open={isDialogOpen} onClose={closeDialog} aria-labelledby="form-dialog-title">
-                        <DialogTitle id="form-dialog-title">Get Started</DialogTitle>
-                        <DialogContent>
-                        <DialogContentText>
-                            Enter the UUID of a folder you want to navigate to.
-                        </DialogContentText>
-                        <TextField
-                            id="input-with-icon-textfield"
-                            fullWidth
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        <FolderIcon />
-                                    </InputAdornment>
-                                ),
-                            }}
-                            label="Folder UUID"
-                            onChange={handleFolderUUIDChange}
-                            onKeyPress={handleTextFieldKeyPress}
-                            value={folderUUID}
-                            variant="outlined"
-                        />
-                        </DialogContent>
-                        <DialogActions>
-                        <Button 
-                            color="primary" 
-                            disabled={isNavigateButtonDisabled} 
-                            onClick={handleNavigate} 
-                            variant="contained"
-                        >
-                            Navigate
+                {isLoadingNewFolder ? (
+                    <Backdrop open>
+                        <CircularProgress />
+                    </Backdrop>
+                ) : (
+                    <div className={classes.home}>
+                        <Button color="primary" onClick={openDialog} variant="outlined">
+                            Get Started
                         </Button>
-                        </DialogActions>
-                    </Dialog>
-                </div>   
+                        <GetStartedDialog 
+                            folderUUID={folderUUID}
+                            handleCreateNewFolder={handleCreateNewFolder}
+                            handleFolderUUIDChange={handleFolderUUIDChange}
+                            handleNavigate={handleNavigate}
+                            onClose={closeDialog}
+                            open={isDialogOpen} 
+                        />
+                    </div>   
+                )}
             </Content>
         </div>
     );
--- src/utilities/ApiUtil.js
+++ src/utilities/ApiUtil.js
@@ -5,4 +5,9 @@ export class ApiUtil {
         const { data } = await axios.get(`/api/folders/${uuid}`);
         return data;
     }
+
+    static async createFolder(name, parentFolderUUID) {
+        const { data } = await axios.post(`/api/folders`, { name, parentFolderUUID });
+        return data;
+    }
 }
