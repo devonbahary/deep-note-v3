ad2890b962cb88e7d6bd0fe5f04e0d8045fa395b
Devon Bahary <devon.bahary@robinhealthcare.com>
2021-05-07 13:53:56 -0700

    adds debounced save note text call

--- src/note/NoteEditor.js
+++ src/note/NoteEditor.js
@@ -1,24 +1,31 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ReactQuill from 'react-quill';
+import { ApiUtil } from '../utilities/ApiUtil';
+import { useDebounce } from '../utilities/react-hooks';
 import 'react-quill/dist/quill.snow.css'; 
 import './note-editor.css';
-import { ApiUtil } from '../utilities/ApiUtil';
 
 export const NoteEditor = ({ note }) => {
     const { uuid } = useParams();
 
     const [ content, setContent ] = useState(note.text);
-    const reactQuillRef = useRef();
+    const editorHTMLRef = useRef();
+
+    const saveNoteText = () => ApiUtil.updateNoteText(uuid, editorHTMLRef.current);
+    const debouncedSaveNoteText = useDebounce(saveNoteText, 100);
+    
+    useEffect(() => {
+        if (editorHTMLRef.current) debouncedSaveNoteText();
+    }, [ content ]);
 
     const handleChange = async (content, delta, source, editor) => {
         setContent(content);
-        await ApiUtil.updateNoteText(uuid, editor.getHTML());
+        editorHTMLRef.current = editor.getHTML();
     }
 
     return (
         <ReactQuill 
-            ref={reactQuillRef}
             onChange={handleChange}
             value={content}
         />
